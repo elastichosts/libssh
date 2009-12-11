66762cb7d91af47eccb8424a70c30317ea457167
Laurent Bigonville <bigon@bigon.be>
2009-12-11 10:53:45 +0100

    Imported Upstream version 0.4.0

--- include/libssh/libssh.h
+++ include/libssh/libssh.h
@@ -50,6 +50,7 @@
 
 #ifdef _MSC_VER
   /* Visual Studio hasn't inttypes.h so it doesn't know uint32_t */
+  typedef int int32_t;
   typedef unsigned int uint32_t;
   typedef unsigned short uint16_t;
   typedef unsigned char uint8_t;
